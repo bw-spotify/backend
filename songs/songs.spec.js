6c5808bb2ae67e4b5b8572fb83597a7a198d55e6
mcferrenm <mcferrenmax@gmail.com>
2019-03-11 18:15:42 -0400

    Add tests to /api/songs

---
+++ songs/songs.spec.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+
+const server = require("../api/server.js");
+const Songs = require("./songs-model.js");
+
+describe("/api/songs", () => {
+  describe("routes", () => {
+    describe("GET /", () => {
+      it("should return 200", async () => {
+        const res = await request(server).get("/api/songs");
+
+        expect(res.status).toBe(200);
+      });
+      it("should return a list of songs", async () => {
+        const songs = await request(server).get("/api/songs");
+
+        expect(songs.body).toBeInstanceOf(Array);
+      });
+    });
+  });
+  describe("Songs Model", () => {
+    describe("get()", () => {
+      it("should return songs from the db", async () => {
+        const songs = await Songs.get();
+
+        expect(songs).toBeDefined();
+      });
+    });
+  });
+});
