a24d8511347b382d7db83dbc82c11c2aa48ded1f
mcferrenm <mcferrenmax@gmail.com>
2019-03-11 16:31:02 -0400

    /api/songs returns songs and song by id

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const usersRouter = require("../users/users-router.js");
 const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const songsRouter = require("../songs/songs-router.js");
 
 const server = express();
 
@@ -11,7 +12,8 @@ server.use(helmet());
 server.use(express.json());
 
 // Routes
-server.use("/api/users", usersRouter);
 server.use("/api", authRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/songs", songsRouter);
 
 module.exports = server;
