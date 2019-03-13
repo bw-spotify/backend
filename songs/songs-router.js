562f60ffc7bc65e5909152a4a49865df9a87597e
mcferrenm <mcferrenmax@gmail.com>
2019-03-13 14:48:01 -0400

    /api/songs GET by id returns similar song track and artist names

--- songs/songs-router.js
+++ songs/songs-router.js
@@ -1,10 +1,28 @@
 const router = require("express").Router();
-
+const _ = require("lodash");
 const Songs = require("./songs-model");
 
 router.get("/", async (req, res) => {
   try {
     const songs = await Songs.get(req.query);
+    const simArray = _.values(songs.similars);
+
+    const a = await Promise.all(
+      simArray
+        .map(async sim => {
+          let data = await Songs.get({ id: sim });
+
+          return {
+            artist_name: data.artist_name,
+            track_name: data.track_name,
+            id: data.id
+          };
+        })
+        .filter((song, index) => index !== 0)
+    );
+
+    songs.similars = a;
+
     res.status(200).json(songs);
   } catch (error) {
     res.status(500).json({ error: "Error querying songs" });
@@ -14,6 +32,7 @@ router.get("/", async (req, res) => {
 router.get("/search", async (req, res) => {
   try {
     const songs = await Songs.search(req.query);
+
     res.status(200).json(songs);
   } catch (error) {
     res.status(500).json({ error: "Error searching songs" });
